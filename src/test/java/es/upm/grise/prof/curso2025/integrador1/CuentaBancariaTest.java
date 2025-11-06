d9b98a4eef1b29bff7c2149d7778aa41408c225f
dandan <d.liang@alumnos.upm.es>
2025-11-06 12:27:07 +0100

    DL - cuentaTest

---
+++ src/test/java/es/upm/grise/prof/curso2025/integrador1/CuentaBancariaTest.java
@@ -0,0 +1,64 @@
+package es.upm.grise.prof.curso2025.integrador1;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.*;
+import org.mockito.Mockito;
+
+public class CuentaBancariaTest {
+    private CuentaBancaria cuenta;
+
+    @BeforeEach
+    public void setUp(){
+        cuenta = new CuentaBancaria("1234", 5);
+    }
+
+    @Test
+    public void nuevaCuentaNoOperacionTest(){
+        assertEquals(0, cuenta.operaciones.size());
+    }
+
+    @Test
+    public void operacionNulaTest(){
+        assertThrows(OperationNulaException.class, () -> {cuenta.addOperacion(null);});
+    }
+
+    @Test
+    public void operacionDuplicadaTest() throws OperationNulaException, OperationDuplicadaException{
+        Operacion op = Mockito.mock(Operacion.class);
+        Mockito.when(op.getId()).thenReturn(10L);
+        Mockito.when(op.getImporte()).thenReturn(15.0);
+        cuenta.addOperacion(op);
+        assertThrows(OperationDuplicadaException.class, ()->{cuenta.addOperacion(op);});
+    }
+
+    @Test
+    public void operacionAddCorrectamenteTest() throws OperationNulaException,OperationDuplicadaException{
+        Operacion op = Mockito.mock(Operacion.class);
+        Mockito.when(op.getId()).thenReturn(10L);
+        Mockito.when(op.getImporte()).thenReturn(15.0);
+        Operacion op2 = Mockito.mock(Operacion.class);
+        Mockito.when(op2.getId()).thenReturn(11L);
+        Mockito.when(op2.getImporte()).thenReturn(15.0);
+        cuenta.addOperacion(op);
+        cuenta.addOperacion(op2);
+        assertEquals(2, cuenta.operaciones.size());
+    }
+
+    @Test
+    public void operacionNegativaNoAdmiteTest(){
+        Operacion op = Mockito.mock(Operacion.class);
+        Mockito.when(op.getId()).thenReturn(10L);
+        Mockito.when(op.getImporte()).thenReturn(-20.0);
+        cuenta.admiteDescubierto = false;
+        assertThrows(SaldoNegativoException.class, ()->{cuenta.getSaldoActual();});
+    }
+
+    @Test
+    public void OperacionNegativaAdmiteTest()throws SaldoNegativoException{
+        Operacion op = Mockito.mock(Operacion.class);
+        Mockito.when(op.getId()).thenReturn(10L);
+        Mockito.when(op.getImporte()).thenReturn(-20.0);
+        cuenta.admiteDescubierto = true;
+        assertEquals(-15.0, cuenta.getSaldoActual());
+    }
+}
